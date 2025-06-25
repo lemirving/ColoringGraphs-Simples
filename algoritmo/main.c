517b15807377e82a940f2421e73d0759642a16c5
Irving <dev.lemirving@gmail.com>
2025-06-25 13:45:10 -0500

    Atualização do projeto para entrega

--- algoritmo/main.c
+++ algoritmo/main.c
@@ -4,14 +4,11 @@
 #include <time.h>
 #include "grafo.h"
 
-
 typedef struct {
     int sat;
     int degree;
     int indice;
 } infoVertice;
-
-
 
 int compararInfoVertice(const void *a, const void *b) {
     infoVertice *na = (infoVertice *)a;
@@ -19,16 +16,18 @@ int compararInfoVertice(const void *a, const void *b) {
 
     if (na->sat != nb->sat)
         return nb->sat - na->sat;
-
     if (na->degree != nb->degree)
         return nb->degree - na->degree;
-
     return na->indice - nb->indice;
 }
+
+
 
 void ordenarFila(infoVertice *queue, int size) {
     qsort(queue, size, sizeof(infoVertice), compararInfoVertice);
 }
+
+
 
 void removerPrimeiro(infoVertice *queue, int *size) {
     for (int i = 1; i < *size; i++)
@@ -36,14 +35,31 @@ void removerPrimeiro(infoVertice *queue, int *size) {
     (*size)--;
 }
 
+
+
 int contarCores(int *vetorCores, int tam) {
     int max = 0;
     for (int i = 0; i < tam; i++) {
         if (vetorCores[i] > max)
             max = vetorCores[i];
     }
-    return max + 1; 
+    return max + 1;
 }
+
+bool validarColoracao(Grafo *g, int *cor) {
+    for (int u = 0; u < g->tam; u++) {
+        for (int i = 0; i < g->vertices[u].grau; i++) {
+            int v = g->vertices[u].vizinhos[i];
+            if (cor[u] == cor[v]) {
+                printf("Conflito entre vértices %d e %d (cor %d)\n", u, v, cor[u]);
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
 
 void Dsatur(Grafo *g) {
     int cor[g->tam];
@@ -108,17 +124,25 @@ void Dsatur(Grafo *g) {
         ordenarFila(q_infos, queue_size);
     }
 
-    printf("\nColoração final:\n");
-    for (int i = 0; i < g->tam; i++) {
-        printf("Vértice %d ---> Cor %d\n", i, cor[i]);
-    }
-
     int totalCores = contarCores(cor, g->tam);
     printf("\nNúmero total de cores usadas: %d\n", totalCores);
+
+    for (int c = 0; c < totalCores; c++) {
+        printf("COR %d: ", c);
+        for (int i = 0; i < g->tam; i++) {
+            if (cor[i] == c)
+                printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    if (!validarColoracao(g, cor)) {
+        printf("\nColoração inválida! Há vértices vizinhos com a mesma cor.\n");
+    } else {
+        printf("\nColoração válida.\n");
+    }
 }
 
-
-//Leitura do grafo
 int carregarGrafo(char *nomeArquivo, Grafo *g) {
     FILE *arquivo = fopen(nomeArquivo, "r");
     if (arquivo == NULL) {
@@ -152,42 +176,41 @@ void liberarGrafo(Grafo *g) {
     free(g->vertices);
 }
 
-void descricaoArquivo(char *nomeArquivo){
+void descricaoArquivo(char *nomeArquivo) {
     char linha[100];
     FILE *arquivo = fopen(nomeArquivo, "r");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.\n");
- 
+        return;
     }
+
     printf("\n");
-    char c;  
-    while (fgets(linha,sizeof(linha),arquivo) != NULL) {
-        if(linha[0] == 'c' || linha[0] == 'p'){
-            printf("%s", linha+2);
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        if (linha[0] == 'c' || linha[0] == 'p') {
+            printf("%s", linha + 2);
         }
     }
     fclose(arquivo);
 }
 
 int main() {
-    clock_t start,end;
+    clock_t start, end;
     double time_used;
 
     start = clock();
 
     Grafo g;
-    char nomeArquivo[] = "instancia/inithx.i.1.col";
-    
+    char nomeArquivo[] = "instancia/queen15_15.col";
+
     descricaoArquivo(nomeArquivo);
 
     if (carregarGrafo(nomeArquivo, &g)) {
         Dsatur(&g);
         liberarGrafo(&g);
     }
+
     end = clock();
-
-    time_used = ((double) (end-start) / CLOCKS_PER_SEC);
-
+    time_used = ((double)(end - start) / CLOCKS_PER_SEC);
     printf("Tempo de execução: %.2lfs\n", time_used);
     return 0;
 }
